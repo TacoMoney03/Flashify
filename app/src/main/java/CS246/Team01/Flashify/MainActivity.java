a46f2efd703daf4c065c1a95e9c01c2008c78710
tmearns <65202387+TacoMoney03@users.noreply.github.com>
2020-06-15 16:59:08 -0400

    Initial commit

---
+++ app/src/main/java/CS246/Team01/Flashify/MainActivity.java
@@ -0,0 +1,14 @@
+package CS246.Team01.Flashify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
