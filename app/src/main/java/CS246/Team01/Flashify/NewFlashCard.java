eb4b263f961f0cb2a87b359e50a685a81432d7f8
jonjaira <jonjaira@gmail.com>
2020-06-17 02:11:13 -0400

    Upward button added

--- app/src/main/java/CS246/Team01/Flashify/NewFlashCard.java
+++ app/src/main/java/CS246/Team01/Flashify/NewFlashCard.java
@@ -7,7 +7,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
