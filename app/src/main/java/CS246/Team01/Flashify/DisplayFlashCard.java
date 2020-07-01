7fbcc92adb43d97ac7cfe97c3a0eb5ea10909ae4
filipecamargos <filipe.atrabalho@gmail.com>
2020-06-30 19:22:31 -0600

    next buttom

--- app/src/main/java/CS246/Team01/Flashify/DisplayFlashCard.java
+++ app/src/main/java/CS246/Team01/Flashify/DisplayFlashCard.java
@@ -18,11 +18,20 @@ public class DisplayFlashCard extends AppCompatActivity {
     private TextView frontView;
     private TextView backView;
 
+    ArrayList<FlashCard> topicFlashcards;
+
+
+    int index = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_flash_card);
+        setCardText();
 
+    }
+
+    public void setCardText() {
         // grab all views by ID on create
         frontView = findViewById(R.id.frontTextView);
         backView = findViewById(R.id.backTextView);
@@ -31,18 +40,18 @@ public class DisplayFlashCard extends AppCompatActivity {
         _intent = getIntent();
 
         // get the "front" and "back" strings into the attributes
-        front = _intent.getStringExtra("FRONT");
-        back = _intent.getStringExtra("BACK");
+        //front = _intent.getStringExtra("FRONT");
+        //back = _intent.getStringExtra("BACK");
 
         //Getting the list
-        ArrayList<FlashCard> topicFlashcards =  (ArrayList<FlashCard>) getIntent().getSerializableExtra("mylist");
+        topicFlashcards =  (ArrayList<FlashCard>) getIntent().getSerializableExtra("mylist");
         Log.i("LIST TESTING", "topicFlashcards.toString()");
         Log.i("LIST TESTING", topicFlashcards.toString());
         System.out.println(topicFlashcards);
 
         // set the flashcard front to show "front" and the flashcard back to show "back"
-        frontView.setText(front);
-        backView.setText(back);
+        frontView.setText(topicFlashcards.get(index).get_front());
+        backView.setText(topicFlashcards.get(index).get_back());
 
         // set the back portion of the flashcard to be invisible on create
         backView.setVisibility(View.INVISIBLE);
@@ -54,21 +63,28 @@ public class DisplayFlashCard extends AppCompatActivity {
         if (View.INVISIBLE == frontView.getVisibility()){
             backView.setVisibility(View.INVISIBLE);
             frontView.setVisibility(View.VISIBLE);
-            frontView.setText(front);
+            //frontView.setText(front);
         }
         else{
             frontView.setVisibility(View.INVISIBLE);
             backView.setVisibility(View.VISIBLE);
-            backView.setText(back);
+           // backView.setText(back);
         }
-
-
-
     }
 
     public void nextCard(View view) {
-        frontView.setText("Front view test 1");
-        backView.setText("Back view test 1");
+
+        System.out.println("Index: " + index + "Legnth: " + (topicFlashcards.size() - 1));
+        if(!(index >= topicFlashcards.size() - 1 )) {
+            ++index;
+            setCardText();
+            System.out.println(index + "Made ++");
+        } else {
+            System.out.println( index + "Made else");
+            index = 0;
+            setCardText();
+            System.out.println( index + "Made else");
+        }
     }
 
     public void previousCard(View view) {
